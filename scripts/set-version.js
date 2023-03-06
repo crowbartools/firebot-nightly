c731fc187f84d740e49ffc79ae0a73ddddf34dca
SReject <SReject@users.noreply.github.com>
2023-03-06 12:13:45 -0500

    import path dep

--- scripts/set-version.js
+++ scripts/set-version.js
@@ -1,5 +1,6 @@
 const version = process.argv.slice(2).join(' ');
 const { readFileSync, writeFileSync } = require('fs');
+const path = require('path');
 
 const packagePath = path.resolve(__dirname, '../../package.json');
 package.version = version;
