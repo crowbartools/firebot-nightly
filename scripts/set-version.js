15b9b4b13b5ca1ebc490e55df436312d301ce120
SReject <SReject@users.noreply.github.com>
2023-03-06 11:43:14 -0500

    clone nightly into own dir

--- scripts/set-version.js
+++ scripts/set-version.js
@@ -1,7 +1,7 @@
 const version = process.argv.slice(2).join(' ');
 const { readFileSync, writeFileSync } = require('fs');
 
-const packagePath = path.resolve(__dirname, '../package.json');
+const packagePath = path.resolve(__dirname, '../../package.json');
 package.version = version;
 const package = JSON.parse(readFileSync(packagePath, 'utf-8'));
 
